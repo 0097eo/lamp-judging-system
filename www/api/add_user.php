9914922b56c2f8a160d0ac27df0e3431b0a81b59
0097eo <emmanuelokello294@gmail.com>
2025-05-23 20:42:41 +0300

    Add API endpoint to add a user with input validation

---
+++ www/api/add_user.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../config/database.php';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = $_POST['username'] ?? '';
+    $display_name = $_POST['display_name'] ?? '';
+    if ($username && $display_name) {
+        $db = new Database();
+        $db->addUser($username, $display_name);
+        echo json_encode(['status' => 'success']);
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'Invalid input']);
+    }
+}
